932c05e03d099c49c84f6842abf9c79205ad34c6
vivekChenna <chennavivek96@gmail.com>
2023-08-16 22:43:46 +0530

    added boiler code to all the components

---
+++ src/components/Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div>this is the login page</div>;
+};
+
+export default Login;
---
+++ src/components/Signup.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Signup = () => {
+  return <div>this is the signup page</div>;
+};
+
+export default Signup;
